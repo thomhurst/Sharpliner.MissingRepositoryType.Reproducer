d3a636dd67a1d0f447fa855a896f47fcb954c2b9
Tom Longhurst <tom.longhurst@asos.com>
2023-04-05 10:51:19 +0100

    Commit reproducable example

---
+++ Sharpliner.MissingRepositoryType.Reproducer/TestPipeline.cs
@@ -0,0 +1,23 @@
+﻿using Sharpliner.AzureDevOps;
+
+namespace Sharpliner.MissingRepositoryType.Reproducer;
+
+public class TestPipeline : SingleStagePipelineDefinition
+{
+    public override string TargetFile { get; } = "foo.yml";
+
+    public override SingleStagePipeline Pipeline { get; } = new()
+    {
+        Trigger = new Trigger("main"),
+        Resources = new Resources
+        {
+            Repositories =
+            {
+                new RepositoryResource("sharpliner")
+                {
+                    Name = "sharpliner/sharpliner", Type = RepositoryType.Git
+                }
+            }
+        }
+    };
+}
